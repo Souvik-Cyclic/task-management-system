eee56a2951fa19f5f025bafaa8f74297a1176ff7
Souvik Kumar <souvik.cyc@gmail.com>
2024-08-21 18:17:52 +0530

    Add task routes to server

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 require('dotenv').config();
 require('./config/dbConfig')
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

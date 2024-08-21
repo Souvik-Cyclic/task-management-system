134b494eefb8da22d396931609816c9f8492ec28
Souvik Kumar <souvik.cyc@gmail.com>
2024-08-21 19:25:39 +0530

    Add dotenv and set Sequelize dialect

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres'
+});
 
 sequelize.authenticate()
   .then(() => console.log('Database connected successfully.'))

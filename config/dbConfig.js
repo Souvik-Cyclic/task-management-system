160ddf4d98e0ac8b8493ae44237f148d8810bf07
Souvik Kumar <souvik.cyc@gmail.com>
2024-08-21 20:41:53 +0530

    Disable Sequelize logging and add database synchronization

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  logging: false
 });
 
 sequelize.authenticate()
   .then(() => console.log('Database connected successfully.'))
   .catch(err => console.error('Failed to connect to the database:', err));
+
+sequelize.sync({ force: false })
 
 module.exports = sequelize;
